dbaf8434da37ced6b7c5ba8cf8f0cea8bfe4053a
fauziramdaniii <fauziramdhan38@gmail.com>
2024-01-28 13:51:10 +0700

    Add View For Input Search And Show Weather Today

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
+import background from './assets/background.jpg';
+
 
 function App() {
-
   return (
-    <>
-    <h1> Create Weather App </h1>
-    </>
+    <div className="app" style={{ backgroundImage: `url(${background})` }}>
+        <div className="overlay">
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name='city' placeholder='Entere a City Name' />
+              <button type='submit'> ℃ </button>
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3> Jakarta, ID</h3>
+                <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+                <h3> Cloudy </h3>
+              </div>
+              <div className='temperature'>
+             <h1> 34 ℃ </h1>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </div>
   )
 }
 
